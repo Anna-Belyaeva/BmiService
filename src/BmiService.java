3bb6d20025b1a85946a4239397e7a711cb5229e0
Anna Belyaeva <anya.belyaeva.1998@list.ru>
2023-03-23 20:05:25 +0300

    Math.pow

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,7 @@
 public class BmiService {
     public int calculate (int weightKg, double heightMeter){
 
-        double height2 = heightMeter * heightMeter;
+        double height2 = Math.pow(heightMeter, 2);
         double index = weightKg / height2;
 
         return (int)index;
